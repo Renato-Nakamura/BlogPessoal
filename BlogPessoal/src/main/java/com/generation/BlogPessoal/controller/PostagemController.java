45ad890834ddc5778ba0b227382726148587bd71
Renato-Nakamura <renato.yukio30@hotmail.com>
2021-05-27 14:10:27 -0300

    Ativ 2 blog pessoal

--- BlogPessoal/src/main/java/com/generation/BlogPessoal/controller/PostagemController.java
+++ BlogPessoal/src/main/java/com/generation/BlogPessoal/controller/PostagemController.java
@@ -3,11 +3,17 @@ package com.generation.BlogPessoal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.BlogPessoal.model.Postagem;
@@ -33,6 +39,21 @@ public class PostagemController {
 	@GetMapping("/titulo/{titulo}")
 	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
 		return ResponseEntity.ok(repository.findOneByTituloContainingIgnoreCase(titulo));
+	}
+	@PostMapping
+	@ResponseStatus(code= HttpStatus.CREATED)
+	public Postagem post (@RequestBody Postagem postagem){
+		return repository.save(postagem);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> putsgrila (@RequestBody Postagem postagem){
+		postagem = repository.save(postagem);
+		return ResponseEntity.status(HttpStatus.OK).body(postagem);
+	}
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 	
 }
